2fa647a1787b0d0beef932bf96b49421f0298ab0
yangzx <597032797@qq.com>
2020-03-19 16:23:11 +0800

    压缩图片并上传图片

--- pages/uploadFile/uploadFile.js
+++ pages/uploadFile/uploadFile.js
@@ -1,13 +1,17 @@
-//logs.js
 const {
   UploadFile
 } = require('../../utils/api.js')
+const {
+  imageUtil
+} = require('../../utils/util.js')
 const app = getApp()
 Page({
   data: {
     logs: [],
     fileList: [],
-    gallery: false
+    gallery: false,
+    cw: 100,
+    ch: 100
   },
   onLoad: function () {
 
@@ -16,14 +20,49 @@ Page({
     const {
       file
     } = event.detail;
-    let _this = this
+    this.getCanvasImg(file)
+  },
+  getCanvasImg: function (tempFile) {//压缩图片
+    var that = this;
     wx.showToast({
-        icon: "loading",
-        title: "正在上传"
-      }),
+      icon: "loading",
+      title: "正在上传"
+    })
+    wx.getImageInfo({
+      src: tempFile.path, //图片的路径，可以是相对路径，临时文件路径，存储文件路径，网络图片路径,  
+      success: res => {
+        var imageSize = imageUtil(res);
+        const ctx = wx.createCanvasContext('attendCanvasId');
+        setTimeout(() => {
+          ctx.drawImage(tempFile.path, 0, 0, imageSize.imageWidth, imageSize.imageHeight);
+          that.setData({
+            cw: imageSize.imageWidth,
+            ch: imageSize.imageHeight
+          });
+          ctx.draw(true, function () {
+            wx.canvasToTempFilePath({
+              canvasId: 'attendCanvasId',
+              success: function success(res) {
+                that.uploadfile(res.tempFilePath,tempFile)             
+              },
+              fail: function (e) {
+                console.log('失败回调')
+              }
+            });
+          });
+        }, 500);
+
+      },
+      fail: () => {},
+      complete: () => {}
+    });
+  },
+  uploadfile(filePath,file){//上传图片
+    let _this=this
+    
       wx.uploadFile({
         url: UploadFile,
-        filePath: file.path,
+        filePath: filePath,
         name: 'file',
         header: {
           "Content-Type": "multipart/form-data",
@@ -42,7 +81,6 @@ Page({
           _this.setData({
             fileList
           });
-
         },
         fail: function (e) {
           wx.showModal({
@@ -55,26 +93,6 @@ Page({
           wx.hideToast(); //隐藏Toast
         }
       })
-
-  },
-  close: function () {
-    this.setData({
-      gallery: false,
-    });
-  },
-  open: function () {
-    this.setData({
-      gallery: true
-    });
-  },
-  delImg: function () {
-    this.setData({
-      gallery: false,
-      files: [],
-    });
-  },
-  onPullDownRefresh() {
-    console.info('123')
 
   }
 })
